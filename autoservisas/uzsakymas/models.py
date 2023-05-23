662c5410860e4f745048e8de4793d987ab86f65b
MPythonS <starkus.mantas@gmail.com>
2023-05-23 21:40:43 +0300

    settings.py itraukti appsai, uzsakymai/models.py klses Paslauga ir Uzsakymo_eilute

--- autoservisas/uzsakymas/models.py
+++ autoservisas/uzsakymas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Paslauga (models.Model):
+    pavadinimas = models.CharField(max_length=255)
+    kaina = models.IntegerField()
+
+class Uzsakymo_eilute (models.Model):
+      Paslauga = models.ForeignKey(Paslauga)
+    # Uzsakymas = models.ForeignKey(Uzsakymas)
+    Kiekis = models.CharField(max_length=255)
+    Kaina = models.CharField(max_length=255)
+
