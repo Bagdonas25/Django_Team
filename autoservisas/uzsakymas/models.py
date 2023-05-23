09c3594b1e33fb8e62f221b3335191324d950da7
Tomas Puza <tomas.puza@gmail.com>
2023-05-23 21:39:11 +0300

    uzsakymo appsas supildyta

--- autoservisas/uzsakymas/models.py
+++ autoservisas/uzsakymas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from automobilis_id.models import automobilis
 
 # Create your models here.
+class Uzsakymas(models.Model):
+
+    data = models.CharField(max_length=255)
+    automobilis_id = models.ForeignKey(
+        automobilis,
+
+    )
+    suma = models.IntegerField()
