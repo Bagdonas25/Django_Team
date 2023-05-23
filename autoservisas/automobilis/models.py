ccc0653012aa438f2132943061fade667dc8aba5
bagdonas <k.bagdonavicius@gmail.com>
2023-05-23 21:43:06 +0300

    sukurtos dvi klases

--- autoservisas/automobilis/models.py
+++ autoservisas/automobilis/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
-automobilis
-automobilio_modelis
+class Automobilio_modelis(models.Model):
+    Marke = models.CharField(max_length=100)
+    Modelis = models.CharField(max_length=100)
+
+
+class Automobilis (models.Model):
+
+    Valstybinis_NR = models.CharField (max_length=6, help_text="ABC123")
+    Automobilio_modelis_id = models.ForeignKey (Automobilio_modelis)
+    VIN_kodas = models.CharField (max_length=30)
+    klientas = models.CharField (max_length=100)
+
